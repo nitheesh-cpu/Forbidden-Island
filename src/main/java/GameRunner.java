0dbc65fb2edcf4a12bb5a6179e56f0864136e919
nitheesh-cpu <nitishkoda@outlook.com>
2021-11-03 12:13:48 -0500

    Fixed GameRunner.java

--- src/main/java/GameRunner.java
+++ src/main/java/GameRunner.java
@@ -1,5 +1,4 @@
 import com.formdev.flatlaf.intellijthemes.FlatNordIJTheme;
-import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -7,18 +6,8 @@ import java.io.IOException;
 public class GameRunner {
 
     public static void main(String[] args) {
-//        FlatDarculaLaf.setup();
-//        FlatAtomOneDarkContrastIJTheme.setup();
-        FlatNordIJTheme.setup();
-//        FlatDarkLaf.setup();
-
-        try {
-            FileUtils.deleteDirectory(new File("BingoCards/cards"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        FlatNordIJTheme.install();
 
         MenuPanel bingoGame = new MenuPanel();
-
     }
 }
