dbf122e7413c572f6d534261105c7fec4a8dcfea
slhscs299 <gordonj2016@outlook.com>
2021-11-10 12:13:11 -0600

    commit added

--- src/main/java/game/simulation/GameState.java
+++ src/main/java/game/simulation/GameState.java
@@ -8,30 +8,56 @@ import game.simulation.card.Card;
 import game.simulation.player.Player;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Stack;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class GameState {
     private GameBoard board;
     private int waterLevel;
     private TreasurePiece[] treasuresCollected;
     private int numPlayers;
-    private String[] allRoles;
-    private final ArrayList<Player> allPlayers;
-    private static int playerTurn = 0;
+    private final String[] allRoles;
+    private ArrayList<Player> allPlayers;
+    private int playerTurn = 0;
     private Player currentPlayer;
     private WaterLevelMeter meter;
     private ArrayList<Card> currentDeck;
-    private static Stack<Card> cardDeck;
-    private static Stack<Card> discardPile;
+    private static Stack<String> cardDeck;
+    private static Stack<String> discardPile;
     private ArrayList<GameTile> moveableSpaces;
+    Iterator<Player> playerIterator;
 
     public GameState(int difficulty, int numPlayers) {
         this.numPlayers = numPlayers;
         meter = new WaterLevelMeter();
         waterLevel = meter.getWaterLevel();
-
+        allRoles = new String[]{"Navigator", "Messenger", "Engineer", "Pilot", "Explorer", "Diver"};
+        Random rnd = ThreadLocalRandom.current();
+        for (int i = allRoles.length - 1; i > 0; i--)
+        {
+            int index = rnd.nextInt(i + 1);
+            String a = allRoles[index];
+            allRoles[index] = allRoles[i];
+            allRoles[i] = a;
+        }
+        cardDeck = new Stack<>();
+        for(int i = 0; i < 4; i++) cardDeck.push("CrystalOfFire");
+        for(int i = 0; i < 4; i++) cardDeck.push("StatueOfWind");
+        for(int i = 0; i < 4; i++) cardDeck.push("OceansChalice");
+        for(int i = 0; i < 4; i++) cardDeck.push("EarthStone");
+        for(int i = 0; i < 3; i++) cardDeck.push("HelicopterLift");
+        for(int i = 0; i < 2; i++) cardDeck.push("Sandbag");
+        Collections.shuffle(cardDeck);
+        for(int i = 0; i < numPlayers; i++) {
+            ArrayList<String> startingDeck = new ArrayList<>();
+            startingDeck.add(cardDeck.pop());
+            startingDeck.add(cardDeck.pop());
+            Player p = new Player(allRoles[i], startingDeck);
+            allPlayers.add(p);
+        }
+        for(int i = 0; i < 3; i++) cardDeck.push("WatersRise");
+        Collections.shuffle(cardDeck);
+        playerIterator = allPlayers.iterator();
     }
 
     public void shuffle(Stack<Card> pile) {
@@ -42,13 +68,16 @@ public class GameState {
     }
 
     public void setWater(int waterLevel) {
+        this.waterLevel = waterLevel;
     }
 
     public void raiseWater() {
+        ++waterLevel;
     }
 
     public Player nextTurn() {
-        return
+        if(!playerIterator.hasNext()) playerIterator = allPlayers.iterator();
+        return playerIterator.next();
     }
 
     public void drawFlood(Graphics g) {
@@ -60,9 +89,6 @@ public class GameState {
 
     public boolean checkLosing() {
         return true;
-    }
-
-    public void showHelp() {
     }
 
     public ArrayList<GameTile> findMovable() {
--- src/main/java/game/simulation/player/Player.java
+++ src/main/java/game/simulation/player/Player.java
@@ -1,4 +1,19 @@
 package game.simulation.player;
 
+import game.simulation.board.GameTile;
+import game.simulation.card.Card;
+
+import java.util.ArrayList;
+
 public class Player {
+    private ArrayList<String> playerDeck;
+    private boolean deckFilled;
+    private String role;
+    private static int moveNumber = 0;
+    private boolean hasSunk;
+    private GameTile position;
+    public Player(String role, ArrayList<String> startingDeck) {
+        this.role = role;
+        playerDeck = startingDeck;
+    }
 }
