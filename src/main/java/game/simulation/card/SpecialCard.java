78aa653e5977e97424afec894bd30ea6a173b713
yuiy0311 <luckykitty0311@gmail.com>
2021-11-10 12:16:57 -0600

    instantiate buffered images for special card

--- src/main/java/game/simulation/card/SpecialCard.java
+++ src/main/java/game/simulation/card/SpecialCard.java
@@ -1,13 +1,18 @@
 package game.simulation.card;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
 
 public class SpecialCard implements Card {
     private String type;
     private BufferedImage image;
 
-    private SpecialCard(String x) {
+    private SpecialCard(String x) throws IOException {
         type = x;
-//        image = Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("Images"));
+        if(x == "helicopter") image = ImageIO.read(getClass().getClassLoader().getResource("Images/TreasureCards/Card_Helicopter.png"));
+        if(x == "sandbag")    image = ImageIO.read(getClass().getClassLoader().getResource("Images/TreasureCards/Card_Sand_Bag.png"));
+
     }
 }
