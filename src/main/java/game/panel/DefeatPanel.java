af53e4f00de6fc0037bbe1d64957622e2f59204f
BruceYale <yule.bryce@gmail.com>
2021-11-12 11:54:59 -0600

    started Defeat and Victory Panels, and changed the calls to them inside WinCondition

--- src/main/java/game/panel/DefeatPanel.java
+++ src/main/java/game/panel/DefeatPanel.java
@@ -1,4 +1,33 @@
 package game.panel;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.util.Objects;
 
-public class DefeatPanel {
+public class DefeatPanel extends JFrame
+{
+    private Font PNBold;
+    private int WIDTH = 300, HEIGHT = 300;
+
+    public DefeatPanel(String title)
+    {
+        super(title);
+        setResizable(false);
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        try
+        {
+            PNBold = Font.createFont(Font.TRUETYPE_FONT, Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("Fonts/Proxima Nova Bold.otf"))).deriveFont(25f);
+        } catch (FontFormatException | IOException e) {
+            System.out.println("Error");
+        }
+    }
+
+    public void paint(Graphics g)
+    {
+        g.setFont(PNBold);
+        g.drawString("YOU LOSE!", getWidth()/2, getHeight()/2);
+    }
 }
--- src/main/java/game/panel/VictoryPanel.java
+++ src/main/java/game/panel/VictoryPanel.java
@@ -1,4 +1,33 @@
 package game.panel;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.util.Objects;
 
-public class VictoryPanel {
+public class VictoryPanel extends JFrame
+{
+    private Font PNBold;
+    private int WIDTH = 300, HEIGHT = 300;
+
+    public VictoryPanel(String title)
+    {
+        super(title);
+        setResizable(false);
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        try
+        {
+            PNBold = Font.createFont(Font.TRUETYPE_FONT, Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("Fonts/Proxima Nova Bold.otf"))).deriveFont(25f);
+        } catch (FontFormatException | IOException e) {
+            System.out.println("Error");
+        }
+    }
+
+    public void paint(Graphics g)
+    {
+        g.setFont(PNBold);
+        g.drawString("YOU WIN!", getWidth()/2, getHeight()/2);
+    }
 }
