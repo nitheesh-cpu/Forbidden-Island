f48b9e95a0ebdb8c33dc833aa13858ce7507756c
yuiy0311 <luckykitty0311@gmail.com>
2021-11-12 12:02:43 -0600

    create engineer constructor and moveableTiles

--- src/main/java/game/simulation/player/Engineer.java
+++ src/main/java/game/simulation/player/Engineer.java
@@ -1,4 +1,19 @@
 package game.simulation.player;
 
+import game.panel.GameBoard;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+
 public class Engineer {
+    private ArrayList<ArrayList<Integer>>  moveableTiles;
+
+    public Engineer(){
+        moveableTiles = new ArrayList<>();
+    }
+
+    public ArrayList<ArrayList<Integer>> getMoveableTiles(int[] pos){
+      //  if(GameBoard.getAllTiles())
+        return moveableTiles;
+    }
 }
