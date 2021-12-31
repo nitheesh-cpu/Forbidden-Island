0fad76e0756a069f6ad14893296d7e5931b1ef88
nitheeshcpu <nitishkoda@outlook.com>
2021-12-30 18:37:09 -0600

    boop

--- src/main/java/game/simulation/card/Card.java
+++
@@ -1,9 +0,0 @@
-package game.simulation.card;
-
-import java.awt.image.BufferedImage;
-
-public interface Card {
-
-    public String getType();
-    public BufferedImage getImage();
-}
--- src/main/java/game/simulation/player/Diver.java
+++
@@ -1,4 +0,0 @@
-package game.simulation.player;
-
-public class Diver {
-}
